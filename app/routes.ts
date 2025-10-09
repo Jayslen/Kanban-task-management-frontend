2c9f4ddb3aa63296cf19a09b0326c3a1becad0dd
jayslen <jayslen134@gmail.com>
2025-10-09 18:52:27 -0400

    fix: standardize route file casing for consistency

--- app/routes.ts
+++ app/routes.ts
@@ -3,9 +3,9 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 export default [
     layout('./layouts/BasicLayout.tsx', [
         layout('./layouts/BoardLayout.tsx', [
-            index("routes/Home.tsx"),
+            index("routes/home.tsx"),
             route('board/:boardId', 'routes/Board.tsx'),
         ]),
-        route('login', 'routes/login.tsx'),
+        route('login', 'routes/Login.tsx'),
     ]),
 ] satisfies RouteConfig;
